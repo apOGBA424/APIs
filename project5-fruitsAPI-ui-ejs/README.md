6473cc8e5610642ad4b48131d1157d8290362a28
Paschal A. Ogba <paxbond01@gmail.com>
2023-05-08 22:21:48 +0100

    initail node and install ejs

--- project5-fruitsAPI-ui-ejs/README.md
+++ project5-fruitsAPI-ui-ejs/README.md
@@ -1,18 +1,22 @@
  PROJECT NAME  : <b>```project5-fruitsAPI-ui-ejs```</b>
 
-##### (<i>a fake User API </i>)
+##### (<i>a fake Fruit API </i>)
 
 <h5>
     <details>
         <summary>TOOLS</summary>
-        HTML | CSS | Javascript |   NodeJs | Express | EJS
+        HTML | CSS | EJs
     </details>
 </h5> <br><hr><br><br>
 
 ## DESCRIPTION
 
-This is a fifth project in my series of learning to create API.<br>However, it is not a continuation of my first API, [<b>x-api</b>](https://github.com/apOGBA424/APIs/tree/main/x-api 'Click to visit " x-api " github repository').<br><br>
+This fifth section is a continuation from the [<b>fourth</b>](https://github.com/apOGBA424/APIs/tree/main/project4-fruitsAPI-ui) project.<br> Here I initialized **node** then, installed and import [<b>EJs</b>](https://www.npmjs.com/package/ejs), a **NodeJs**<br> dependency for injecting dynamic data to an html tag.<br>
+
+However, note that the pages were not designed to be<br> responsive, thus will appear broken on mobile phone browser and<br> other screen browsers less than 1000px in width.<br><br>
 
 ### FEATURES
-Though this project shares same technologies as seen in the<br> previous API, however few changes were made by the<br> introduction of [***Mongoose***](https://www.npmjs.com/package/mongoose 'Click to learn more'), a third-party NPM package for<br> MongoDB, to replace MongoClient( ) object that comes with<br> MongoDB package. <br><br>
+A simple five pages website as show in image<br> below written in HTML, CSS and EJs.<br><br>
 
+<img  src='./public/images/fruitsapi-poster.png' width=420px height=400px alt='fruit-api-ui'>
+<br><br>
--- project5-fruitsAPI-ui-ejs/app.js
+++ project5-fruitsAPI-ui-ejs/app.js
@@ -6,7 +6,6 @@ Steps:
 2. set-up server port
 3. declare variables
 4. set middlewares
-5. test database connection
 */
 // ******************************
 
@@ -15,19 +14,62 @@ Steps:
 const express = require('express');
 const app = express();
 
+
 // variables
 const PORT = process.env.PORT || 3000;
 
-// middlewares
-// db connection
+
+// ******* middlewares *******
+// static files and folder paths
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
+// set views
+app.set('view engine', 'ejs');
+// app.set('views', './views');
 
 
-// routes-and-logics
+
+// ********** routes-and-logics **********
+// home page
 app.get('/', (req, res)=>{
-    console.log('app running OK');
+    console.info('app running OK');
     // res.send('hello world on browser');
-    res.sendFile('./views/index.html', {root: __dirname});
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index', {title0: 'Home' });
 });
+
+// dashboard page
+app.get('/dashboard', (req, res)=>{
+    console.info(`dashboard page running on port ${PORT}`);
+    res.render('dashboard', {title1 : 'Dashboard'});
+});
+
+// details page
+app.get('/details', (req, res)=>{
+    console.info(`details page running on port ${PORT}`);
+    res.render('details', {title2 : 'Details'});
+});
+
+// sign-in page
+app.get('/sign-in', (req, res)=>{
+    console.info(`sign-in page running on port ${PORT}`);
+    res.render('sign-in', {title3 : 'Sign-in'});
+});
+
+// sign-out page
+app.get('/sign-up', (req, res)=>{
+    console.info(`sign-out page running on port ${PORT}`);
+    res.render('sign-up', {title4 : 'Sign-Up'});
+});
+
+// 404 page
+app.use((req, res)=>{
+    console.info(`404 page running on port ${PORT}.`);
+    res.render('404', {title5 : '404 page'});
+});
+
 
 
 app.listen(PORT, ()=>{
